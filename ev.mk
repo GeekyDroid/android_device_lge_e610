41cb054651650530cac8321da15d75e071d2ed8d
aidasaidas75 <aidaslukosius75@gmail.com>
2014-04-27 21:03:37 +0300

    ev

---
+++ ev.mk
@@ -0,0 +1,42 @@
+# Inherit device configuration
+$(call inherit-product, device/lge/e610/full_e610.mk)
+
+# Inherit some common evervolv stuff.
+$(call inherit-product, vendor/ev/config/common_full_phone.mk)
+$(call inherit-product, vendor/ev/config/gsm.mk)
+
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
+
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := e610
+PRODUCT_NAME := ev_e610
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-E610
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_CHARACTERISTICS := phone
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=m4_open_eu \
+    BUILD_FINGERPRINT="lge/m4_open_eu/m4:4.1.2/JZO54K/E61020c-EUR-XX.1367460723:user/release-keys" \
+    PRIVATE_BUILD_DESC="m4_open_eu-user 4.1.2 JZO54K E61020c-EUR-XX.1367460723 release-keys"
+
+PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your LGE E610\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
+
+# Copy compatible prebuilt files
+#PRODUCT_COPY_FILES += \
+# vendor/ev/prebuilt/hvga/media/bootanimation.zip:system/media/bootanimation.zip
+
+
+
+
+
+
+
+
+
+
+
+
+
